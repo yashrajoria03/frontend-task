17847bc3dcdfa3f9b2bcc410801718350d939b12
Yash Rajoria <yashkhatri826@gmail.com>
2023-12-05 16:10:20 +0530

    added context api

--- components/Result.jsx
+++ components/Result.jsx
@@ -1,6 +1,9 @@
 import React from "react";
+import SearchContext from "@context/searchContext";
 
 const Result = () => {
+  const [state, dispatch] = useContext(SearchContext);
+
   return <div className="h-[100vh]">Result</div>;
 };
 
